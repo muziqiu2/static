37defd68e73c612dc516342a38dc33a91e6189a3
muziqiu2 <2443539514@qq.com>
2024-06-22 18:53:49 +0800

    个人博客静态文件更新

---
+++ js/blog/random.js
@@ -0,0 +1,1 @@
+var posts=["posts/3145788926/","posts/1475875489/","posts/125/","posts/16827/","posts/30169/","posts/1751468545/","posts/926119936/","posts/1985886148/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random()*posts.length)]);};
